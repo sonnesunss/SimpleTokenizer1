22307b0767f94b07e4a248fc01b89695760d853d
sun <145575806+sonnesunss@users.noreply.github.com>
2025-05-09 03:03:00 +0800

    Update tokenizer.rs
    1.  移除不必要的input字段
    
    2. 添加单元测试代码

--- simple_tokenizer/src/tokenizer.rs
+++ simple_tokenizer/src/tokenizer.rs
@@ -2,6 +2,7 @@ use std::str::Chars;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
+#[derive(PartialEq)]
 pub enum Token {
     Number(i32),
     Plus,
@@ -18,7 +19,6 @@ pub enum Token {
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct Tokenizer<'a> {
-    input: &'a str,
     chars_iter: std::iter::Peekable<Chars<'a>>,
 }
 
@@ -26,7 +26,6 @@ pub struct Tokenizer<'a> {
 impl<'a> Tokenizer<'a> {
     pub fn new(input: &'a str) -> Self {
         Tokenizer {
-            input,
             chars_iter: input.chars().peekable(),
         }
     }
@@ -50,11 +49,11 @@ impl<'a> Tokenizer<'a> {
         match self.chars_iter.next() {
             Some(c) => Some(
                 self.match_char(c)
-            ), 
+            ),
             None => None
         }
     }
-    
+
     fn match_char(&self, c: char) -> Token {
         match c {
             '+' => Token::Plus,
@@ -65,7 +64,7 @@ impl<'a> Tokenizer<'a> {
             '(' => Token::LParen,
             ')' => Token::RParen,
             '0'..='9' => Token::Number(c.to_string().parse::<i32>().unwrap()),
-            _ => Token::Error("Unexpected char -> {c}".to_owned()), 
+            _ => Token::Error(format!("Unexpected char -> {}", c)),
         }
     }
 }
@@ -78,3 +77,81 @@ impl<'a> Iterator for Tokenizer<'a> {
         self.next_token()
     }
 }
+
+////////////////
+// unit test
+///////////////
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_char_single_symbols() {
+        let tokenizer = Tokenizer::new("");
+        assert_eq!(tokenizer.match_char('+'), Token::Plus);
+        assert_eq!(tokenizer.match_char('-'), Token::Minus);
+        assert_eq!(tokenizer.match_char('*'), Token::Multiply);
+        assert_eq!(tokenizer.match_char('/'), Token::Divide);
+        assert_eq!(tokenizer.match_char('^'), Token::Power);
+        assert_eq!(tokenizer.match_char('('), Token::LParen);
+        assert_eq!(tokenizer.match_char(')'), Token::RParen);
+    }
+
+    #[test]
+    fn test_match_char_digit() {
+        let tokenizer = Tokenizer::new("");
+        assert_eq!(tokenizer.match_char('0'), Token::Number(0));
+        assert_eq!(tokenizer.match_char('5'), Token::Number(5));
+        assert_eq!(tokenizer.match_char('9'), Token::Number(9));
+    }
+
+    #[test]
+    fn test_match_char_unexpected() {
+        let tokenizer = Tokenizer::new("");
+        let token = tokenizer.match_char('x');
+        match token {
+            Token::Error(msg) => assert!(msg.contains("x")),
+            _ => panic!("Expected error token"),
+        }
+    }
+
+    #[test]
+    fn test_next_token_basic() {
+        let mut tokenizer = Tokenizer::new(" + - * / ^ ( ) 1 2 3");
+        assert_eq!(tokenizer.next_token(), Some(Token::Plus));
+        assert_eq!(tokenizer.next_token(), Some(Token::Minus));
+        assert_eq!(tokenizer.next_token(), Some(Token::Multiply));
+        assert_eq!(tokenizer.next_token(), Some(Token::Divide));
+        assert_eq!(tokenizer.next_token(), Some(Token::Power));
+        assert_eq!(tokenizer.next_token(), Some(Token::LParen));
+        assert_eq!(tokenizer.next_token(), Some(Token::RParen));
+        assert_eq!(tokenizer.next_token(), Some(Token::Number(1)));
+        assert_eq!(tokenizer.next_token(), Some(Token::Number(2)));
+        assert_eq!(tokenizer.next_token(), Some(Token::Number(3)));
+        assert_eq!(tokenizer.next_token(), None);
+    }
+
+    #[test]
+    fn test_iterator_trait() {
+        let tokens: Vec<Token> = Tokenizer::new("1 + 2").collect();
+        assert_eq!(tokens, vec![
+            Token::Number(1),
+            Token::Plus,
+            Token::Number(2),
+        ]);
+    }
+
+    #[test]
+    fn test_skip_whitespace() {
+        let mut tokenizer = Tokenizer::new("   +   ");
+        assert_eq!(tokenizer.next_token(), Some(Token::Plus));
+        assert_eq!(tokenizer.next_token(), None);
+    }
+
+    #[test]
+    fn test_empty_input() {
+        let mut tokenizer = Tokenizer::new("");
+        assert_eq!(tokenizer.next_token(), None);
+    }
+}
+
